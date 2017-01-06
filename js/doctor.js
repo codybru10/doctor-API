9eafe3dad2a233f9d9e9ab556a21063149cc8127
Cody Brubaker <cody.brubaker@gmail.com>
2017-01-06 11:55:24 -0800

    list first name

--- js/doctor.js
+++ js/doctor.js
@@ -1,33 +1,49 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor() {}
+function Doctor() {
 
+}
 
-var arrayOfDoctors = [];
-
-Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDoctor = function(issue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ issue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(response) {
-     debugger;
+      console.log(response);
       $('#countDoctors').append("<li>" + response.data.length + "</li>");
-      console.log(response.data.length);
+      // console.log(response.data)
       response.data.forEach(function(doctor) {
-        doctor.practices.forEach(function(practice){
-        console.log(practice.name);
-        $('#showDoctors').append("<li>" + practice.name + "</li>");
-        });
+        var name = doctor.profile.first_name;
+            $('#showDoctors').append("<li>" + name + "</li>");
+            console.log(name);
+    
       });
-
-
-      // arrayOfDoctors.push(data)
-      // console.log(arrayOfDoctors)
-      // console.log(arrayOfDoctors.data.profile.first_name)
     })
-   .fail(function(error){
-      console.log("fail");
-    });
 
-};
+   .fail(function(error){
+      $('#countDoctors').text(error.responseJSON.message);
+    });
+  };
+
+
+
+// Doctor.prototype.getDoctors = function(medicalIssue) {
+//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+//    .then(function(response) {
+//      debugger;
+    //   $('#countDoctors').append("<li>" + response.data.length + "</li>");
+    //   console.log(response.data.length);
+    //   response.data.forEach(function(doctor) {
+    //     doctor.practices.forEach(function(practice){
+    //       practice.insurance_uids.forEach(function(id){
+    //         $('#showDoctors').append("<li>" + id + "</li>");
+    //       });
+    //     });
+    //   });
+    // })
+//    .fail(function(error){
+//       console.log("fail");
+//     });
+//
+// };
 
 
 
--- js/doctore-interface.js
+++ js/doctore-interface.js
@@ -2,10 +2,11 @@ var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
-  var test_result = new Doctor();
   $('#medical-issue-form').submit(function(event) {
     event.preventDefault();
+    var doctorsObject = new Doctor();
     var issue = $('#issue').val();
-    test_result.getDoctors(issue);
+    $('#condition').text("Your issue is " + issue + ".");
+    doctorsObject.getDoctor(issue);
   });
 });
